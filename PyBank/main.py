20b62701d8d757647f22cc4d8ef7e97ac789dc33
Molly_888 <magcox5@yahoo.com>
2019-11-30 19:58:58 -0800

    Budget Summary Output to screen and file

--- PyBank/main.py
+++ PyBank/main.py
@@ -22,16 +22,9 @@ with open(budget_csv, 'r') as csvfile:
     average_profit_change = 0.0
     greatest_increase_profit = 0.0
     greatest_decrease_profit = 0.0
-    greatest_month_increase = ''
-    greatest_month_decrease = ''
 
     # Split the data on commas
     csvreader = csv.reader(csvfile, delimiter=',')
-
-    # Each row is one month, so total months is row count - 1 for header
-    # total_months = sum(1 for row in csvreader)-1
-    # print(f"Total # of months = {total_months}")
-
 
     greatest_increase_month = datetime.date(2000, 1, 1)
     greatest_decrease_month = datetime.date(2000, 1, 1)
@@ -47,38 +40,44 @@ with open(budget_csv, 'r') as csvfile:
 
         # test to see if difference between current and last
         # row profit is greater than previous increase/decrease profit value
-        row_difference = float(row[1]) - last_row
-        if row_difference > greatest_increase_profit:
-            greatest_increase_profit = row_difference
-            greatest_increase_month = row[0]
-        elif row_difference < greatest_decrease_profit:
-            greatest_decrease_profit = row_difference
-            greatest_decrease_month = row[0]
+        if total_months <= 1:
+            last_row = float(row[1])
+        if total_months > 1:
+            row_difference = float(row[1]) - last_row
+            if row_difference > greatest_increase_profit:
+                greatest_increase_profit = row_difference
+                greatest_increase_month = row[0]
+            elif row_difference < greatest_decrease_profit:
+                greatest_decrease_profit = row_difference
+                greatest_decrease_month = row[0]
 
-        last_row = float(row[1])
-        sum_profit_change += row_difference
+            last_row = float(row[1])
+            sum_profit_change = sum_profit_change + row_difference
 
 
     print("Financial Analysis  [Negative Numbers in ()]")
     print("-----------------------------------------------------")
     print(f"Total Months:  {total_months}")
     print(f"Total:  {locale.currency(total_profit, grouping = True)}")
-    print(f"Average Change: {locale.currency(sum_profit_change/total_months, grouping = True)}")
+    print(f"Average Change: {locale.currency(sum_profit_change/(total_months-1), grouping = True)}")
     print(f"Greatest Profit Increase: {greatest_increase_month} {locale.currency(greatest_increase_profit, grouping = True)}")
     print(f"Greatest Profit Decrease: {greatest_decrease_month} {locale.currency(greatest_decrease_profit, grouping = True)}")
 
-    # Write summary data to txt file
+    orig_stdout = sys.stdout
     filename  = open("budget_summary",'w')
     sys.stdout = filename
+
+    # Write summary data to txt file
     print("Financial Analysis  [Negative Numbers in ()]")
     print("-----------------------------------------------------")
     print(f"Total Months:  {total_months}")
     print(f"Total:  {locale.currency(total_profit, grouping = True)}")
-    print(f"Average Change: {locale.currency(sum_profit_change/total_months, grouping = True)}")
+    print(f"Average Change: {locale.currency(sum_profit_change/(total_months-1), grouping = True)}")
     print(f"Greatest Profit Increase: {greatest_increase_month} {locale.currency(greatest_increase_profit, grouping = True)}")
     print(f"Greatest Profit Decrease: {greatest_decrease_month} {locale.currency(greatest_decrease_profit, grouping = True)}")
 
-    
+    sys.stdout = orig_stdout
+    filename.close()
     
 
         
